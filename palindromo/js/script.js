11ef9f4420901962ee8e7dffb0dbd751c1854122
Alex <adrdra88@gmail.com>
2021-11-16 15:27:13 +0100

    palindromo

---
+++ palindromo/js/script.js
@@ -0,0 +1,41 @@
+
+const parola = prompt('inserire parola');
+
+is_palindrome(parola);
+
+
+
+function check_palindrome( str )
+{
+  let j = str.length -1;
+  for( let i = 0 ; i < j/2 ;i++)
+  {
+    let x = str[i] ;//forward character
+    let y = str[j-i];//backward character
+    if( x != y)
+    {
+      // return false if string not match
+      return false;
+    }
+  }
+  /// return true if string is palindrome
+  return true;
+   
+}
+
+//function that print output is string is palindrome
+function is_palindrome( str )
+{
+  // variable that is true ig string is pallindrome
+  let ans = check_palindrome(str);
+  //condition checking ans is true or not
+  if( ans == true )
+  {
+    console.log("passed string is palindrome ");
+  }
+  else
+  {
+    console.log("passed string not a palindrome");
+  }
+}
+// test variable
